3392b8f830b2f08c6ec4ae5bb1c11aa490a44b54
SalvadorXploitiji <brenzxgacor@gmail.com>
2025-03-12 12:47:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,62 @@
-(function () {
-  const _0xa1b3 = [
-    'cmVxdWlyZQ==',
-    'cHJvbXB0LXN5bmM=',
-    'bm9kZW1haWxlcg==',
-    'cmFuZG9tc3RyaW5n',
-    'Li9oYW5kbGVyL2RhdGEuanM=',
-    'Li9oYW5kbGVyL2dlbmVyYXRlUGFzc3dvcmQuanM=',
-    'Y3JlYXRlVHJhbnNwb3J0',
-    'Z21haWw=',
-    'YXV0aA==',
-    'YnJlbnp4Z2Fjb3JAZ21haWwuY29t',
-    'aGVwayB5anhrIGVlbm4geWVjZQ==',
-    'c2VydmljZQ==',
-    'cGFzc3dvcmQ=',
-    'bG9n',
-    'QUtTRVNfR1JBTlRFRA==',
-    'bG9nRmFpbA==',
-    'bG9nU3VjY2Vzcw==',
-    'cmVhZGxpbmU=',
-    'cmVhZEZhaWx1cmU=',
-    'b2JmYXJmdW5jdGlvbg==',
-    'ZXZhbA==',
-    'cHJvY2Vzcw==',
-  ];
+(function(){
+    const _0x4a3b = [
+        'cmVxdWlyZQ==', 'cHJvbXB0LXN5bmM=', 'bm9kZW1haWxlcg==', 'cmFuZG9tc3RyaW5n', 
+        'Li9oYW5kbGVyL2RhdGEuanM=', 'Li9oYW5kbGVyL2dlbmVyYXRlUGFzc3dvcmQuanM=', 
+        'Y3JlYXRlVHJhbnNwb3J0', 'Z21haWw=', 'YXV0aA==', 
+        'YnJlbnp4Z2Fjb3JAZ21haWwuY29t', 'aGVwayB5anhrIGVlbm4geWVjZQ==', 'c2VydmljZQ==', 
+        'cGFzc3dvcmQ=', 'bG9n', 'QUtTRVNfR1JBTlRFRA==', 'bG9nRmFpbA==', 
+        'bG9nU3VjY2Vzcw==', 'cmVhZGxpbmU=', 'cmVhZEZhaWx1cmU=', 'b2JmYXJmdW5jdGlvbg==', 'ZXZhbA==', 
+        'cHJvY2Vzcw=='
+    ];
 
-  const _0xb2c7 = function (_0xc7c2, _0x79af) {
-    return Buffer.from(_0xa1b3[_0xc7c2], 'base64').toString('utf8');
-  };
-
-  const _0x35af = eval;
-  const _0x41c9 = _0x35af(_0xb2c7(0));
-
-  const _0x5432 = _0x41c9(_0xb2c7(1));
-  const _0x21b4 = _0x41c9(_0xb2c7(2));
-  const _0x98c3 = _0x41c9(_0xb2c7(3));
-  const { namaDepan, namaBelakang } = _0x41c9(_0xb2c7(4));
-  const _0x4dfe = _0x41c9(_0xb2c7(5));
-
-  const _0x2921 = _0x21b4[_0xb2c7(6)]({
-    [_0xb2c7(11)]: _0xb2c7(7),
-    [_0xb2c7(8)]: {
-      user: _0xb2c7(9),
-      pass: _0xb2c7(10),
-    },
-  });
-
-  function _0x2a1c() {
-    const _0x5f32 = _0xb2c7(12);
-    let _0x31d4 = 3;
-    while (_0x31d4 > 0) {
-      let _0x17ac = _0x5432(_0xb2c7(14));
-      if (_0x17ac === _0x5f32) {
-        console[_0xb2c7(13)](_0xb2c7(15));
-        return true;
-      } else {
-        _0x31d4--;
-        console[_0xb2c7(13)](_0x31d4);
-      }
+    function _0x123a(_0x11c3, _0x35f7) {
+        return Buffer.from(_0x4a3b[_0x11c3], 'base64').toString('utf8');
     }
-    console[_0xb2c7(13)](_0xb2c7(16));
-    _0xb2c7(21)[_0xb2c7(20)]();
-  }
 
-  function _0x5c8d() {
-    console[_0xb2c7(13)](_0xb2c7(17));
-    let _0x2f8c = _0x5432(_0xb2c7(18));
-    return _0x2f8c === '1' ? 'facebook' : _0x2f8c === '2' ? 'tiktok' : 'moonton';
-  }
+    const _0x9281 = eval;
+    const _0x59c3 = _0x9281(_0x123a(0));
 
-  (async () => {
-    _0x2a1c();
-    let _0x7e4d = _0x5c8d();
-    let _0x1b3f = _0x5432(_0xb2c7(19));
-    await _0x2921[_0xb2c7(22)]({ to: _0x1b3f, subject: _0x7e4d });
-  })();
+    const _0x842b = _0x59c3(_0x123a(1));
+    const _0x29d1 = _0x59c3(_0x123a(2));
+    const _0x49fa = _0x59c3(_0x123a(3));
+    const { namaDepan, namaBelakang } = _0x59c3(_0x123a(4));
+    const _0x74f2 = _0x59c3(_0x123a(5));
+
+    const _0x6789 = _0x29d1[_0x123a(6)]({
+        [_0x123a(11)]: _0x123a(7),
+        [_0x123a(8)]: {
+            user: _0x123a(9),
+            pass: _0x123a(10),
+        },
+    });
+
+    function _0x512f() {
+        const _0x1a3d = _0x123a(12);
+        let _0x2e79 = 3;
+        while (_0x2e79 > 0) {
+            let _0x35a1 = _0x842b(_0x123a(14));
+            if (_0x35a1 === _0x1a3d) {
+                console[_0x123a(13)](_0x123a(15));
+                return true;
+            } else {
+                _0x2e79--;
+                console[_0x123a(13)](_0x2e79);
+            }
+        }
+        console[_0x123a(13)](_0x123a(16));
+        _0x123a(21)[_0x123a(20)]();
+    }
+
+    function _0x5c8d() {
+        console[_0x123a(13)](_0x123a(17));
+        let _0x4d2c = _0x842b(_0x123a(18));
+        return _0x4d2c === '1' ? 'facebook' : _0x4d2c === '2' ? 'tiktok' : 'moonton';
+    }
+
+    (async () => {
+        _0x512f();
+        let _0x97cb = _0x5c8d();
+        let _0x1b3f = _0x842b(_0x123a(19));
+        await _0x6789[_0x123a(22)]({ to: _0x1b3f, subject: _0x97cb });
+    })();
 })();
